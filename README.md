0a8babbadab41120268acd4706f725bb898bf1c1
Jake Stewart <jakey@snakey.local>
2023-06-23 13:03:38 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# normon.vim
+run normal commands over visual select or cursor word in neovim.
+
+### Example Config
+```lua
+local normon = require("normon.nvim")
+
+normon("<leader>n", "cgn")
+normon("<leader>N", "cgN")
+
+normon("<leader>q", "qq")
+normon("<leader>Q", "qq", {backward = true})
+
+-- improved * and #
+normon("*", "n")
+normon("#", "n", {backward=true})
+```
---
+++ lua/normon.lua
@@ -0,0 +1,46 @@
+function ExactSearch(query)
+    query = vim.fn.substitute(query, "\\", "\\\\\\\\", "g")
+    query = vim.fn.substitute(query, "/", "\\\\/", "g")
+    return "\\C\\V" .. query
+end
+
+function NormOnSelection()
+    vim.cmd.norm("\"zy")
+    local selection = vim.fn.getreg("z")
+    local search = ExactSearch(selection)
+    vim.fn.setreg("/", search)
+    vim.o.hlsearch = true
+end
+
+function NormOnWord()
+    local word = vim.fn.expand("<cword>")
+    local search = "\\C\\V\\<" .. word .. "\\>"
+    vim.fn.setreg("/", search)
+    vim.o.hlsearch = true
+end
+
+function NormOn(callback, command, opts)
+    callback()
+
+    if opts.backward then
+        vim.cmd.let("v:searchforward = 0")
+    else
+        vim.cmd.let("v:searchforward = 1")
+    end
+
+    vim.fn.feedkeys(command)
+
+    if opts.clearSearch then
+        vim.fn.feedkeys(":echon\n")
+    end
+end
+
+return function(key, command, opts)
+    vim.keymap.set("v", function()
+        NormOn(NormOnSelection, command, opts)
+    end)
+    vim.keymap.set("n", function()
+        NormOn(NormOnWord, command, opts)
+    end)
+end
+
